e6757c5f8db8e6d6be60f29cfec654115ba4b0f0
Minchan Kim <minchan@kernel.org>
2012-06-08 15:39:27 +0900

    staging: zram: remove special handle of uncompressed page
    xvmalloc can't handle PAGE_SIZE page so that zram have to
    handle it specially but zsmalloc can do it so let's remove
    unnecessary special handling code.
    
    Quote from Nitin
    "I think page vs handle distinction was added since xvmalloc could not
    handle full page allocation. Now that zsmalloc allows full page
    allocation, we can just use it for both cases. This would also allow
    removing the ZRAM_UNCOMPRESSED flag. The only downside will be slightly
    slower code path for full page allocation but this event is anyways
    supposed to be rare, so should be fine."
    
    1. This patch reduces code very much.
    
     drivers/staging/zram/zram_drv.c   |  104 +++++--------------------------------
     drivers/staging/zram/zram_drv.h   |   17 +-----
     drivers/staging/zram/zram_sysfs.c |    6 +--
     3 files changed, 15 insertions(+), 112 deletions(-)
    
    2. change pages_expand with bad_compress so it can count
       bad compression(above 75%) ratio.
    
    3. remove zobj_header which is for back-reference for defragmentation
       because firstly, it's not used at the moment and zsmalloc can't handle
       bigger size than PAGE_SIZE so zram can't do it any more without redesign.
    
    Cc: Seth Jennings <sjenning@linux.vnet.ibm.com>
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Acked-by: Nitin Gupta <ngupta@vflare.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	drivers/staging/zram/zram_sysfs.c

--- drivers/staging/zram/zram_sysfs.c
+++ drivers/staging/zram/zram_sysfs.c
@@ -179,8 +179,7 @@ static ssize_t mem_used_total_show(struct device *dev,
 
 	down_read(&zram->init_lock);
 	if (zram->init_done) {
-		val = zs_get_total_size_bytes(zram->mem_pool) +
-			((u64)(zram->stats.pages_expand) << PAGE_SHIFT);
+		val = zs_get_total_size_bytes(zram->mem_pool);
 	}
 	up_read(&zram->init_lock);
 
