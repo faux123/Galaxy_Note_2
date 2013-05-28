fce18185e1950cb5fdfee057be6a46b577d739d1
Paul Reioux <reioux@gmail.com>
2013-05-27 23:26:44 -0500

    Overclock & Voltage Control: Clean up existing interface from cherry-pick
    Signed-off-by: Paul Reioux <reioux@gmail.com>

---
+++ arch/arm/mach-exynos/cpufreq_common.h
@@ -0,0 +1,20 @@
+/*
+ * Author: Paul Reioux aka Faux123 <reioux@gmail.com>
+ *
+ * Copyright 2013 Paul Reioux
+ * Copyright 2012 Paul Reioux
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#define CPUFREQ_LEVEL_END	(L17 + 1) 
+#define CPU_UV_MV_MAX		1500000
+#define CPU_UV_MV_MIN		600000
