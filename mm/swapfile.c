cd9d06ae6d61b800318e645d9c0d2ec870faf323
Paul Reioux <reioux@gmail.com>
2013-06-11 16:54:16 -0500

    swapfile.c: fix compilation error due to API update
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -2142,7 +2142,7 @@ int swapon(char *name, int swap_flags)
 	if (swap_flags & SWAP_FLAG_PREFER)
 		prio =
 		    (swap_flags & SWAP_FLAG_PRIO_MASK) >> SWAP_FLAG_PRIO_SHIFT;
-	enable_swap_info(p, prio, swap_map);
+	enable_swap_info(p, prio, swap_map, frontswap_map_get(p));
 
 	printk(KERN_INFO "Adding %uk swap on %s.  "
 	       "Priority:%d extents:%d across:%lluk %s%s\n",
